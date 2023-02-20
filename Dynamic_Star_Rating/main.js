6f1a801a7c5640b5affb2758ac8dc946fcbfde4e
Ahmed Kechicheb <ahmedkechicheb13@gmail.com>
2023-02-20 09:58:14 +0100

    Dynamic Stars Rating It has been completed

--- Dynamic_Star_Rating/main.js
+++ Dynamic_Star_Rating/main.js
@@ -2,6 +2,8 @@ const stars = document.querySelectorAll(".stars i");
 stars.forEach((star, index1) => {
   star.addEventListener("click", () => {
     stars.forEach((star, index2) => {
+      // Add the "active" class to the clicked star and any stars with a lower index
+      // and remove the "active" class from any stars with a higher index
       index1 >= index2
         ? star.classList.add("active")
         : star.classList.remove("active");
