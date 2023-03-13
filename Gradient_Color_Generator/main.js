de4ee33052482c167328edcb98e7910a12ffd97d
Ahmed Kechicheb <ahmedkechicheb13@gmail.com>
2023-03-13 11:39:19 +0100

    getRandomColor

--- Gradient_Color_Generator/main.js
+++ Gradient_Color_Generator/main.js
@@ -2,13 +2,17 @@ const gradientBox = document.querySelector(".gradient-box");
 const selectMenu = document.querySelector(".select-box select");
 const colorInputs = document.querySelectorAll(".colors input");
 const textarea = document.querySelector("textarea");
-
+refreshBtn = document.querySelector(".refresh");
+getRandomColor = () => {
+  const randomHex = Math.floor(Math.random * 0xffffff).toString(16);
+  return `#${randomHex}`;
+};
 const generateGradient = () => {
   const gradient = `linear-gradient(${selectMenu.value}, ${colorInputs[0].value}, ${colorInputs[1].value})`;
   gradientBox.style.background = gradient;
-  textarea.value =`background: ${gradient};` ;
+  textarea.value = `background: ${gradient};`;
 };
 colorInputs.forEach((input) => {
   input.addEventListener("input", generateGradient);
 });
-selectMenu.addEventListener("change",generateGradient)
+selectMenu.addEventListener("change", generateGradient);
