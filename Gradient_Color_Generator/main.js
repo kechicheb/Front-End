41872f61d7c1ae1274d9b8e6229bcca1bfdbba3a
Ahmed Kechicheb <ahmedkechicheb13@gmail.com>
2023-03-13 11:35:19 +0100

    added some function

--- Gradient_Color_Generator/main.js
+++ Gradient_Color_Generator/main.js
@@ -1,10 +1,12 @@
 const gradientBox = document.querySelector(".gradient-box");
 const selectMenu = document.querySelector(".select-box select");
 const colorInputs = document.querySelectorAll(".colors input");
+const textarea = document.querySelector("textarea");
 
 const generateGradient = () => {
   const gradient = `linear-gradient(${selectMenu.value}, ${colorInputs[0].value}, ${colorInputs[1].value})`;
   gradientBox.style.background = gradient;
+  textarea.value =`background: ${gradient};` ;
 };
 colorInputs.forEach((input) => {
   input.addEventListener("input", generateGradient);
